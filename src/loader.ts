f41f3b8973548a6a412cd4b014cd8e711c7ad8c6
Sonny T <mail@sonnyt.com>
2020-11-29 18:29:52 -0500

    removed the innerHTML

--- src/loader.ts
+++ src/loader.ts
@@ -6,28 +6,27 @@ u.src = chrome.extension.getURL('uglyemail.js');
 (document.head || document.documentElement).appendChild(u);
 
 const s = document.createElement('style');
-s.type = 'text/css';
-s.innerHTML = `
-  .ade .ugly-email-track-icon {
-    margin-top: -6px;
-    height: 20px;
-    width: 20px;
-    opacity: .6;
-  }
+s.appendChild(document.createTextNode(`
+.ade .ugly-email-track-icon {
+  margin-top: -6px;
+  height: 20px;
+  width: 20px;
+  opacity: .6;
+}
 
-  .ugly-email-track-icon {
-    text-align: center;
-    line-height: 22px;
-    background: white;
-    padding: 0 1px;
-    border-radius: 100%;
-    height: 16px;
-    width: 16px;
-    float: left;
-    margin-right: 5px;
-    position: relative;
-    top: 3px;
-  }
-`;
+.ugly-email-track-icon {
+  text-align: center;
+  line-height: 22px;
+  background: white;
+  padding: 0 1px;
+  border-radius: 100%;
+  height: 16px;
+  width: 16px;
+  float: left;
+  margin-right: 5px;
+  position: relative;
+  top: 3px;
+}
+`));
 
 (document.head || document.documentElement).appendChild(s);
